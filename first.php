4c2da1615ef9ef37510341d42613406a5adfe0a5
Masum <masum2mina@gmai.com>
2020-09-20 16:20:09 +0600

    Second line add on gitlab file

---
+++ first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>gitHub</title>
+</head>
+<body>
+	<?php
+
+	echo "My first GitHub practice";
+	echo "My second line text to check";
+
+	?>
+</body>
+</html>
